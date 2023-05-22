0ebd64947a7ba1c4bb5f5fca5eb54b2d4337e0f5
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-05-22 14:31:16 +0900

    모듈화 완료, 가위바위보 규칙 적용 시작

--- server/public/item.js
+++ server/public/item.js
@@ -1,12 +1,30 @@
-function Item(name) {
-    // 버튼 만들기
-    var buttonEl = document.createElement("button");
-    buttonEl.textContent = name;
+export function Item(key, name, onClick) {
+    this.key = key;
+    this.name = name;
 
-    document.getElementById("item-buttons").append(buttonEl);
+    // 버튼 만들기 => 외부에 감춰야 함(private)
+    this.buttonEl = document.createElement("button");
+    this.buttonEl.textContent = name;
 
     // 클릭 이벤트 핸들링
-    buttonEl.onclick = function() {
-        //
-    }
+    var _self = this;
+    this.buttonEl.onclick = function() {
+        onClick(_self.key); // 여기서 this는 this.buttonEl(호출하는 애)
+    };
+    // this.buttonEl.onclick = onClick; // this가 달라짐
 }
+
+Item.prototype.render = function (parentEl) {
+    parentEl.append(this.buttonEl); // 여기서 this는 item(호출하는 애)
+};
+
+Item.prototype.disable = function (value) {
+    value == true ? 
+        this.buttonEl.setAttribute('disabled', true) : 
+        this.buttonEl.removeAttribute('disabled');
+}
+
+// private으로 만드는 법
+// class Item {
+//     priavte
+// }
--- server/public/main.js
+++ server/public/main.js
@@ -1,87 +1,39 @@
+import { Item } from "./item.js"; // 명시적 모듈화
+
 // 코드 중복이 발생할 수 있는 구조를 방지
 var startEl = document.getElementById("start");
 var comEl = document.getElementById("com");
 
-var items = ["가위", "바위", "보"];
+function game(key) {
+    var com = items[currentIndex];
+    if (key === 'scissors' && com.key === 'scissors') {
+        alert('비겼습니다');
+    }
+}
+
+var itemButtonsEl = document.getElementById("item-buttons");
+var items = [
+    new Item('scissors', "가위", game), 
+    new Item('rock', "바위", game), 
+    new Item('paper', "보", game)
+];
+
+items.forEach(function (item) {
+    item.render(itemButtonsEl);
+    item.disable(true);
+});
+
 var currentIndex = 0;
 var timerId;
 
-var scissorsEl = document.getElementById("scissors");
-var rockEl = document.getElementById("rock");
-var paperEl = document.getElementById("paper");
-
-startEl.removeAttribute('disabled');
-scissorsEl.disabled = true;
-rockEl.disabled = true;
-paperEl.disabled = true;
-
 startEl.onclick = function () {
-    // while, for 등 반복문을 쓰면 너무 빠름!!
-    // 아무 반응 없이 작동 안됨
-    // v8엔진 싱글스레드에서 while을 돌리는 중에
-    // 렌더링(메인스레드에서 수행)을 할 수 없음
-    // 즉, 렌더링할 시간을 주어야 함
+    startEl.setAttribute('disabled', true);
+    items.forEach(function (item) {
+        item.disable(false);
+    });
 
-    // while (true) {
-    //     comEl.textContent = items[0];
-    // }
-
-    // 렌더링할 시간을 줌, 요 시간(ms)마다 안의 function을 실행
     timerId = setInterval(function () {
         currentIndex = ++currentIndex % 3;
-        comEl.textContent = items[currentIndex];
+        comEl.textContent = items[currentIndex].name;
     }, 100);
-    startEl.setAttribute('disabled', true);
-    scissorsEl.removeAttribute('disabled');
-    rockEl.removeAttribute('disabled');
-    paperEl.removeAttribute('disabled');
-}
-
-scissorsEl.onclick = function () {
-    // 얘가 수행되는 중에는 currentIndex가 바뀌지 않을 것 (싱글스레드)
-    var com = items[currentIndex];
-    if (com === "가위") { // 하드코딩
-        alert("비겼습니다");
-    } else if (com === "바위") {
-        alert("졌습니다");
-    } else {
-        alert("이겼습니다");
-    }
-    startEl.removeAttribute('disabled');
-    scissorsEl.disabled = true;
-    rockEl.disabled = true;
-    paperEl.disabled = true;
-    clearInterval(timerId);
-}
-
-rockEl.onclick = function () {
-    var com = items[currentIndex];
-    if (com === "가위") {
-        alert("이겼습니다");
-    } else if (com === "바위") {
-        alert("비겼습니다");
-    } else {
-        alert("졌습니다");
-    }
-    startEl.removeAttribute('disabled');
-    scissorsEl.disabled = true;
-    rockEl.disabled = true;
-    paperEl.disabled = true;
-    clearInterval(timerId);
-}
-
-paperEl.onclick = function () {
-    var com = items[currentIndex];
-    if (com === "가위") {
-        alert("졌습니다");
-    } else if (com === "바위") {
-        alert("이겼습니다");
-    } else {
-        alert("비겼습니다");
-    }
-    startEl.removeAttribute('disabled');
-    scissorsEl.disabled = true;
-    rockEl.disabled = true;
-    paperEl.disabled = true;
-    clearInterval(timerId);
 }
