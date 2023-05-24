9d35f27c8b2f71efb8fe95d97b5ea485cf3e99ae
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-05-24 11:30:07 +0900

    Promise 동기 처리

--- TODO리스트/server/public/sample.js
+++ TODO리스트/server/public/sample.js
@@ -1,22 +1,23 @@
 var sampleControl = Widget.button("sample", {
   label: "샘플",
-  onClick: function () {
-    alert("샘플입니다");
+  onClick: () => {
+    let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
+    promise.then(
+      script => alert(`${script.src}을 불러왔습니다!`),
+      error => alert(`Error: ${error.message}`)
+    );
   }
 })
 document.body.append(sampleControl.el);
 
-function loadScript(src, callback) {
-  let script = document.createElement('script');
-  script.src = src;
-  script.onload = () => callback(null, script);
-  script.onerror = () => callback(new Error(`${src}를 불러오는 도중에 에러가 발생했습니다.`));
-  document.head.append(script);
+function loadScript(src) {
+  return new Promise(function (resolve, reject) {
+    let script = document.createElement('script');
+    script.src = src;
+
+    script.onload = () => resolve(script);
+    script.onerror = () => reject(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+
+    document.head.append(script);
+  });
 }
-
-sampleControl.onClick = loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', (error, script) => {
-  if (error) return handleError(error);
-
-  alert(`${script.src}가 로드되었습니다.`);
-  alert(_); // 스크립트에 정의된 함수
-});
